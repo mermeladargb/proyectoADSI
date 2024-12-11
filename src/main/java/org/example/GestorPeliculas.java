cfa47e9a5e7f5db24d1bfd7f8304442317cac4c1
Delafu7 <128633495+Delafu7@users.noreply.github.com>
2024-12-11 10:37:54 +0100

    Create GestorPeliculas.java

---
+++ src/main/java/org/example/GestorPeliculas.java
@@ -0,0 +1,38 @@
+package proyectoAdsi;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+public class GestorPeliculas {
+	
+	private static GestorPeliculas unGestorpelis=new GestorPeliculas();
+	private ArrayList<Pelicula> pelis;
+	private GestorPeliculas() {
+		pelis=new ArrayList<Pelicula>();
+	}
+	public static GestorPeliculas getGestorPeliculas() {
+		return unGestorpelis;
+	}
+	private Iterator<Pelicula> iterator()
+	{
+		return pelis.iterator();
+	}
+	public JSONArray mostrarPeliculas(String nombrePeli) {
+			Iterator<Pelicula>itr=iterator();
+			JSONArray JSON1= new JSONArray();
+			if (itr.hasNext())
+			{
+				if(itr.next().tieneNombreParecido(nombrePeli)!="") {
+				
+				}
+			}
+			return JSON1;
+	}
+	
+	public Pelicula buscarPeliSeleccionada(int idPeli) {
+		
+	}
+	
+}

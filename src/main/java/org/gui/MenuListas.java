7d6b0f476891b2f35df89de4c2e1e562f60f6d98
mermeladargb <182205137+mermeladargb@users.noreply.github.com>
2024-12-30 10:58:46 +0100

    Creación de listas

--- src/main/java/org/gui/MenuListas.java
+++ src/main/java/org/gui/MenuListas.java
@@ -18,6 +18,9 @@ public class MenuListas extends JFrame {
     private JPanel panelLista;
     private JScrollPane scrollMisListas;
     private JScrollPane scrollLista;
+    private JPanel panelCrearLista;
+    private JTextField nombreLista;
+    private JButton botonMenuCrearLista;
 
     public MenuListas(String username) {
         this.username = username;
@@ -31,33 +34,32 @@ public class MenuListas extends JFrame {
         panelLista = new JPanel();
         scrollLista = new JScrollPane(panelLista);
 
+        panelCrearLista = new JPanel();
+
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
         cardPanel.add(scrollMisListas, "mislistas");
         cardPanel.add(scrollLista, "verlista");
+        cardPanel.add(panelCrearLista, "crearlista");
 
-        JSONObject json = VideoClub.getUnVideoClub().getListasUsuario(username);
-        List<Object> listas = json.getJSONArray("listas").toList();
-
-        JLabel label = new JLabel("Listas de " + username);
-        panelMisListas.add(label);
-
-        if (listas.isEmpty()) {
-            panelMisListas.add(new JLabel("No has creado ninguna lista"));
-        }
-        else {
-            for (Object o : listas) {
-                JButton boton = new JButton((String) o);
-                boton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent actionEvent) {
-                        verLista(boton.getText());
-                        cardLayout.show(cardPanel, "verlista");
-                    }
-                });
-                panelMisListas.add(boton);
+        panelCrearLista.setLayout(new BoxLayout(panelCrearLista, BoxLayout.Y_AXIS));
+        panelCrearLista.add(new JLabel("Nombre de la lista"));
+        nombreLista = new JTextField();
+        panelCrearLista.add(nombreLista);
+        JButton botonCrearLista = new JButton("Crear lista");
+        botonCrearLista.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (nombreLista.getText().isEmpty())
+                    return;
+                VideoClub.getUnVideoClub().crearLista(username, nombreLista.getText());
+                actualizarMisListas();
+                cardLayout.show(cardPanel, "mislistas");
             }
-        }
+        });
+        panelCrearLista.add(botonCrearLista);
+
+        actualizarMisListas();
 
         add(cardPanel);
         cardLayout.show(cardPanel, "mislistas");
@@ -88,5 +90,42 @@ public class MenuListas extends JFrame {
                 cardLayout.show(cardPanel, "mislistas");
             }
         });
+    }
+
+    private void actualizarMisListas() {
+        JSONObject json = VideoClub.getUnVideoClub().getListasUsuario(username);
+        List<Object> listas = json.getJSONArray("listas").toList();
+
+        panelMisListas.removeAll();
+
+        JLabel label = new JLabel("Listas de " + username);
+        panelMisListas.add(label);
+
+        if (listas.isEmpty()) {
+            panelMisListas.add(new JLabel("No has creado ninguna lista"));
+        }
+        else {
+            for (Object o : listas) {
+                JButton boton = new JButton((String) o);
+                boton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        verLista(boton.getText());
+                        cardLayout.show(cardPanel, "verlista");
+                    }
+                });
+                panelMisListas.add(boton);
+            }
+        }
+
+        botonMenuCrearLista = new JButton("Crear lista");
+        botonMenuCrearLista.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                nombreLista.setText("");
+                cardLayout.show(cardPanel, "crearlista");
+            }
+        });
+        panelMisListas.add(botonMenuCrearLista);
     }
 }
